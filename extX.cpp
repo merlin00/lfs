e946e93cdc20e208ab3562b38cd6ab98050dae48
Kideok Kwon <merlin00@pusan.ac.kr>
2012-11-12 22:30:47 -0500

    Add super block, group descriptor and inode.

--- extX.cpp
+++ extX.cpp
@@ -5,10 +5,95 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <dirent.h>
+
+#include <string.h>
+#include <arpa/inet.h>
+
 #include "extX.h"
 
 #define SIZE_OF_BLOCK 4096
+#define MAGIC_OFFSET_OF_SUPERBLOCK 0x38
+
 #define INODES_PER_GROUP 8192
+
+typedef enum _fs_data_type {
+  FS_PADDING,
+  FS_BYTE,
+  FS_WORD,
+  FS_DWORD
+}fs_data_type;
+
+typedef struct _fs_data_struct{
+  fs_data_type type;
+  _dword len;
+}fs_data_struct;
+
+static fs_data_struct SUPERBLOCK_FIELDS[] = {
+  {FS_DWORD, 4}, /* [4 bytes] Inode count */
+  {FS_DWORD, 4}, /* [4 bytes] block count */
+  {FS_DWORD, 4}, /* [4 bytes] res block count */
+  {FS_DWORD, 4}, /* [4 bytes] Free block count */
+  {FS_DWORD, 4}, /* [4 bytes] Free inode count */
+  {FS_DWORD, 4}, /* [4 bytes] First data node block */
+  {FS_DWORD, 4}, /* [4 bytes] Log block size */
+  {FS_DWORD, 4}, /* [4 bytes] Log frag size */
+  {FS_DWORD, 4}, /* [4 bytes] Block per group */
+  {FS_DWORD, 4}, /* [4 bytes] Frag per group */
+  {FS_DWORD, 4}, /* [4 bytes] Inode per group */
+  {FS_DWORD, 4}, /* [4 bytes] mtime */
+  {FS_DWORD, 4}, /* [4 bytes] wtime */
+  {FS_WORD, 2},	 /* [2 bytes] Mount count */
+  {FS_WORD, 2},	 /* [2 bytes] Max mount count */
+  {FS_WORD, 2},	 /* [2 bytes] Magic */
+  {FS_WORD, 2},	 /* [2 bytes] State */
+  {FS_WORD, 2},  /* [3 bytes] Error */
+  {FS_WORD, 2},  /* [3 bytes] Minor version */
+  {FS_DWORD, 4}, /* [4 bytes] Last check */
+  {FS_DWORD, 4}, /* [4 bytes] Check interval */
+  {FS_DWORD, 4}, /* [4 bytes] Creator OS */
+  {FS_DWORD, 4}, /* [4 bytes] Major version */
+  {FS_WORD, 2},	 /* [2 bytes] def_res uid */
+  {FS_WORD, 2},	 /* [2 bytes] def_res gid */
+  {FS_DWORD, 4}, /* [4 bytes] First ino */
+  {FS_WORD, 2},	 /* [2 bytes] Inode size */
+  {FS_WORD, 2},	 /* [2 bytes] Block group number */
+  {FS_DWORD, 4}, /* [4 bytes] Feature compat */
+  {FS_DWORD, 4}, /* [4 bytes] Feature incompat */
+  {FS_DWORD, 4}, /* [4 bytes] feature read only compat */
+  {FS_BYTE, 16}, /* [16 bytes] UUID */
+  {FS_BYTE, 16}, /* [16 bytes] Valume Name */
+  {FS_BYTE, 64}, /* [64 bytes] Last mounted */
+  {FS_DWORD, 4}, /* [4 bytes] Algorithm usage bitmap */
+  {FS_BYTE, 1},	 /* [1 bytes] prealloc block */
+  {FS_BYTE, 1},	 /* [1 bytes] Prealloc dir block */ 
+  {FS_PADDING, 2}, /* [2 bytes] Padding */
+  {FS_BYTE, 16}, /* [16 bytes] Journal uuid */
+  {FS_DWORD, 4}, /* [4 bytes] Jouranl inode number */
+  {FS_DWORD, 4}, /* [4 bytes] Journal dev */
+  {FS_DWORD, 4}, /* [4 bytes] Last orphan  */
+  {FS_BYTE, 16}, /* [16 bytes] Hash seed */
+  {FS_BYTE, 1},	 /* [1 bytes] Hash version and padding */
+  {FS_PADDING, 3}, /* [3 bytes] padding  */
+  {FS_DWORD, 4}, /* [4 bytes] Default mount option */
+  {FS_DWORD, 4}, /* [4 bytes] First meta block group */  
+};
+
+// Group descriptor
+static fs_data_struct GROUP_DESCRIPTOR_FILEDS[] ={
+  {FS_DWORD, 4},     // [4 bytes] Block bitmap
+  {FS_DWORD, 4},     // [4 bytes] Inode bitmap
+  {FS_DWORD, 4},     // [4 bytes] Inode table
+  {FS_WORD, 2},	     // [2 bytes] Free block count
+  {FS_WORD, 2},	     // [2 bytes] Free inode count
+  {FS_WORD, 2},	     // [2 bytes] Used dir conut
+  {FS_PADDING, 2},   // [2 bytes] Padding
+  {FS_PADDING, 12}   // ]12 bytes] Padding
+};
+
+//
+static fs_data_struct I_NODE_FILEDS[] = {
+  {FS_WORD, 2},
+};
 
 int create_block(block_type type, block_des* blk)
 {
@@ -43,26 +128,86 @@ int read_block(int fd, block_des* blk)
   if(fd < 0) return -1;
   return read(fd, blk->block, blk->size);
 }
-/*
-int get_direct_img(int fd, dirent_disk_img* pdir)
+
+int _get_struct_from_block(block_des* blk, 
+			   fs_data_struct* fs_data, 
+			   size_t fs_data_len,
+			   _byte *dset, 
+			   size_t dest_len)
 {
-  ssize_t len = 0;
-  off_t offset = 0;
+  int src_offset = 0;
+  int dest_offset = 0;
+  size_t tot_len = 0;
+  _byte *src = (_byte*)blk->block;
 
-  len = read(fd, pdir, HDR_SIZE_OF_DIRENT);
-  if(len < 0) return len;
+  for(int i = 0 ;  i < fs_data_len ; i++)
+    if(fs_data[i].type != FS_PADDING)
+      tot_len += fs_data[i].len;     
 
-  len = read(fd, pdir->name, pdr->name_len);
-  if(len < 0) return len;
-  
-  offset = lseek(fd, pdir->rec_len, SEEK_CUR);
+  if(tot_len > dest_len) return -1;
+
+  for(int i = 0 ; i < fs_data_len ; i++)
+    {
+      int len = fs_data[i].len;
+
+      switch(fs_data[i].type)
+	{
+	case FS_BYTE:
+	  memcpy((void*)(dset + dest_offset), (void*)(src + src_offset), len);  
+	  dest_offset += len; 
+	  break;
+	case FS_WORD:
+	  *((_word*)(dset + dest_offset)) = ntohs(*((_word*)(src + src_offset)));
+	  dest_offset += len;
+	  break;
+	case FS_DWORD:
+	  *((_dword*)(dset + dest_offset)) = ntohl(*((_dword*)(src + src_offset)));
+	  dest_offset += len;
+	  break;
+	case FS_PADDING:  break;
+	}
+      src_offset += len;
+    }
+
   return 0;
-  }*/
+}
 
-#define BLOCK_GROUP_OF_INODE(inode, inodes_per_group) \
-  (inode - 1) / inodes_per_group
-#define ENTRY_INODE_TABLE(inode, inodes_per_group) \
-  (inode - 1) % inodes_per_group
+int get_superblockfrom(int fd, block_type type, superblock* superblk)
+{
+  int offset = 0;
+  int ret = 0;
+  unsigned int sig = 0;
+  block_des blk;
+  
+  ret = create_block(type, &blk);
+  if(ret < 0) return ret;
+  ret = read_block(fd, &blk);
+  if(ret < 0) return ret;
+
+  // According to block size, find super block.
+  switch(type)
+  {
+  case _1K: ret = read_block(fd, &blk); break;
+  case _2K:
+  case _4K: offset = 1024; break;
+  }
+  if(ret < 0) return ret;
+
+  // Check magic number of super block.
+  sig = *(blk.block + MAGIC_OFFSET_OF_SUPERBLOCK);
+  sig = (sig << 4) | *(blk.block + MAGIC_OFFSET_OF_SUPERBLOCK + 1);
+  if(sig != MAGIC_NUM_OF_SUPERBLOCK)  return -1;
+
+  // Get super block struct from the block.
+  ret = _get_struct_from_block(&blk, 
+			       SUPERBLOCK_FILEDS, 
+			       sizeof(SUPERBLOCK_FILEDS)/sizeof(fs_data_struct),
+			       (_byte*)superblk,
+			       sizeof(superblk));
+  delete_block(&blk);
+
+  return ret;
+}
 
 typedef struct _extX_info {
   unsigned int blks_of_group;	 // Blocks of group
--- extX.h
+++ extX.h
@@ -2,13 +2,13 @@
 #define _EXTX_H_
 
 typedef unsigned short _word;
+typedef unsigned int _dword;
 typedef unsigned char _byte;
 
 typedef enum _block_type{
-  _0k = 0,
-  _1k = 1024,
-  _2k = 2046,
-  _4k = 4096
+  _1K = 1024,
+  _2K = 2046,
+  _4K = 4096
 }block_type;
 
 // File blcok descriptor
@@ -27,6 +27,96 @@ typedef struct _dirent_disk_img{
   _byte typ;			// File Type
   char* name;
 }dirent_disk_img;
+
+/* Superblock magic number 0xEF53 (Big endian) */
+#define MAGIC_NUM_OF_SUPERBLOCK 0xEF53 /* Little endian */
+#define SIZE_OF_SUPERBLOCK 264
+
+/* Linux extX super block */
+typedef struct _superblock {
+  _dword inode_cnt;	/* Inode count */
+  _dword blk_cnt;	/* block count */
+  _dword res_blk_cnt;	/* res block count */
+  _dword free_blk_cnt;	/* Free block count */
+  _dword free_inode_cnt;/* Free inode count */
+  _dword first_data_blk;/* First data node block */
+  _dword log_blk_size;	/* Log block size */
+  _dword log_frag_size;	/* Log frag size */
+  _dword blk_per_grp;	/* Block per group */
+  _dword frag_per_grp;	/* Frag per group */
+  _dword inode_per_grp;	/* Inode per group */
+  _dword mtime;		/* mtime */
+  _dword wtime;		/* wtime */
+  _word mount_cnt;	/* Mount count */ 
+  _word max_mount_cnt;	/* Max mount count */
+  _word magic;		/* Magic */
+  _word state;		/* State */
+  _word err;		/* Error */
+  _word minor;		/* Minor version */
+  _dword last_chk;	/* Last check */
+  _dword chk_interval;	/* Check interval */
+  _dword creator_os;	/* Creator OS */
+  _dword major;		/* Major version */
+  _word def_res_uid;	/* def_res uid */
+  _word def_res_gid;	/* def_res gid */
+  _dword first_ino;	/* First ino */
+  _word inode_size;	/* Inode size */
+  _word blk_grp_num;	/* Block group number */
+  _dword compat;	/* Feature compat */
+  _dword incompat; 	/* Feature incompat */
+  _dword read_only_compat;	/* feature read only compat */
+  _byte uuid[16];		/* UUID [16 bytes] */
+  _byte valume_name[16];	/* Valume Name [16 bytes] */
+  _byte last_mounted[64];	/* Last mounted [64 bytes] */
+  _dword bitmap;		/* Algorithm usage bitmap */
+  _byte prealloc_blk;		/* prealloc block */
+  _byte prealloc_dir;           /* prealloc dir block */
+  _byte journal_uuid[16];	/* Journal uuid */
+  _dword journal_inode_num;	/* Jouranl inode number */
+  _dword journal_dev;		/* Journal dev */
+  _dword last_orphan;		/*  */
+  _byte hash_seed[16];		/* Hash seed */
+  _byte  hash_version;		/* Hash version and padding */
+  _dword default_mount_opt;	/* Default mount option */
+  _dword first_meta_bg;		/* First meta block group */
+}superblock;
+
+/* Group descriptor */
+typedef struct _group_descriptor{
+  _dword blk_bitmap;		/* Block bitmap */
+  _dword inode_bitmap;		/* Inode bitmap */
+  _dword inode_table;		/* Inode table */
+  _word free_blk_cnt;		/* Free block count */
+  _word free_inode_cnt;		/* Free inode count */
+  _word useddir_cnt;		/* Used directory count */
+}group_des;
+fs
+/* Inode */
+typedef struct _i_node {
+  _word mode;
+  _word uid;
+  _dword size;
+  _dword atime;
+  _dword ctime;
+  _dword mtime;
+  _dword dtime;
+  _word gid;
+  _word link_cnt;
+  _dword blocks;
+  _dword flags;
+  _dword osdl;
+  _dword blocks[15];
+  _dword generation;
+  _dword file_acl;
+  _dword dir_acl;
+  _dword faddr;
+  _byte osd2[12];
+}i_node;
+
+#define BLOCK_GROUP_OF_INODE(inode, inodes_per_group) \
+  (inode - 1) / inodes_per_group
+#define ENTRY_INODE_TABLE(inode, inodes_per_group) \
+  (inode - 1) % inodes_per_group
 
 void attach();
 
